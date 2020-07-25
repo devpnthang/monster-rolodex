ccdba991c01fa285ecae75401eaafaa1fae645a7
ThangPham <thangpham.it92@gmail.com>
2020-07-25 16:16:21 +0700

    Updates

---
+++ precache-manifest.e4b15c1823798f8956bff2c7aba02b9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f3d716e64594c91d510cb6fb7c97126",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a2082d946b36392a17b8",
+    "url": "/monsters-rolodex/static/css/main.d1eec17f.chunk.css"
+  },
+  {
+    "revision": "9f834d1fc4df12fb31cd",
+    "url": "/monsters-rolodex/static/js/2.308474c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.308474c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2082d946b36392a17b8",
+    "url": "/monsters-rolodex/static/js/main.a1c5bac6.chunk.js"
+  },
+  {
+    "revision": "0c81cb278443448182f0",
+    "url": "/monsters-rolodex/static/js/runtime-main.165419b0.js"
+  }
+]);
